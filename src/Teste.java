036a1430ed501b8a7d403bc437d016302eadde72
lucasbao <lucasmiranda_1@hotmail.com>
2015-04-26 11:56:57 -0300

    teste lucas

--- src/Teste.java
+++ src/Teste.java
@@ -2,8 +2,8 @@
 public class Teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("teste");
+		
+		System.out.println("aqui é lucas");
 	}
 
 }
