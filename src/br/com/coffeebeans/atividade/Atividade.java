9639e5dd13461ec2153f1f5f4ece524c9c9f2408
Swenilton Souza <susu360graus@hotmail.com>
2015-04-27 20:09:14 -0300

    Criada a classe atividade

---
+++ src/br/com/coffeebeans/atividade/Atividade.java
@@ -0,0 +1,16 @@
+package br.com.coffeebeans.atividade;
+
+import java.sql.Timestamp;
+
+import br.com.coffeebeans.usuario.Usuario;
+
+public class Atividade {
+	
+	private int id;
+	private String descricao;
+	private Timestamp dataHoraInicio;
+	private Timestamp dataHoraFim;
+	private Usuario usuario;
+	
+
+}
