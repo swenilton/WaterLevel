1517a37bf1dd0193abbdfe1779b0aad4c8cdcb2b
RaphaelLorena <raphael@tvasabranca.com.br>
2015-05-24 15:35:35 -0300

    uhu

--- src/br/com/coffeebeans/atividade/AtividadeDAO.java
+++ src/br/com/coffeebeans/atividade/AtividadeDAO.java
@@ -1,16 +1,132 @@
 package br.com.coffeebeans.atividade;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import br.com.coffeebeans.exception.AtividadeNaoEncontradaException;
+import br.com.coffeebeans.exception.RepositorioException;
 import br.com.coffeebeans.util.Conexao;
 
-public class AtividadeDAO {
+
+public class AtividadeDAO implements IAtividadeDAO {
 	
+	private Connection connection = null;
 	private String sistema = "mysql";
-	private Connection conn;
 	
 	public AtividadeDAO() throws Exception {
-		this.conn = new Conexao().conectar(sistema);
+		this.connection = Conexao.conectar(sistema);
+	}
+	
+	public void cadastrar (Atividade atividade) throws SQLException,  
+		RepositorioException {
+		PreparedStatement stmt = null;
+		
+		try {
+			String sql = "INSERT INTO ATIVIDADE (DESCRICAO) VALUES (?)";
+			stmt = this.connection.prepareStatement(sql);
+			stmt.setString(1, atividade.getDescricao());
+			stmt.execute();
+		} catch (SQLException e) {
+			throw new RepositorioException(e);
+		} catch (Exception e) {
+			throw new RepositorioException(e);
+		} finally {
+			stmt.close();
+		}
+	}
+	
+	public List<Atividade> getLista() throws SQLException, RepositorioException {
+		List<Atividade> atividades = new ArrayList<Atividade>();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			String sql = "SELECT * FROM ATIVIDADE";
+			stmt = this.connection.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			while (rs.next()) {
+				Atividade atividade = new Atividade (rs.getString("DESCRICAO"));
+				atividade.setId(rs.getInt("ID"));
+				atividades.add(atividade);
+			}
+		} catch (SQLException e) {
+			throw new RepositorioException(e);
+		} catch (Exception e) {
+			throw new RepositorioException(e);
+		} finally {
+			stmt.close();
+			rs.close();
+		}	
+		return atividades;
+	}
+	
+	public Atividade procurar (int id) throws SQLException, RepositorioException {
+		Atividade atividade = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try {
+			String sql = "SELECT * FROM ATIVIDADE WHERE ID = ?";
+			stmt = this.connection.prepareStatement(sql);
+			stmt.setInt(1, id);
+			rs = stmt.executeQuery();
+			if (rs.next()) {
+				atividade = new Atividade(rs.getString("DESCRICAO"));
+				atividade.setId(rs.getInt("ID"));
+			} else {
+				throw new IllegalArgumentException("Atividade não encontrada");
+			}
+		} catch (SQLException e) {
+			throw new RepositorioException(e);
+		} catch (Exception e) {
+			throw new RepositorioException(e);
+		} finally {
+			stmt.close();
+			rs.close();
+		}
+		return atividade;
+	}
+	
+	public void atualizar(Atividade atividade) throws AtividadeNaoEncontradaException,
+		SQLException, RepositorioException {
+		PreparedStatement stmt = null;
+		try {
+			if (atividade != null) {
+				try {
+					String sql = "";
+					stmt = this.connection.prepareStatement(sql);
+					stmt.setString(1, atividade.getDescricao());
+					stmt.setInt(2, atividade.getId());
+					Integer resultado = stmt.executeUpdate();
+					if (resultado == 0)
+						throw new AtividadeNaoEncontradaException();
+				} catch (SQLException e) {
+					throw new RepositorioException(e);
+				}
+			}
+		} finally {
+			stmt.close();
+		}
+	}
+	
+	public void excluir (int id) throws SQLException, AtividadeNaoEncontradaException,
+		RepositorioException {
+		PreparedStatement stmt = null;
+		try {
+			String sql = "DELETE FROM ATIVIDADE WHERE ID = ?";
+			stmt = this.connection.prepareStatement(sql);
+			stmt.setInt(1, id);
+			stmt.execute();
+		} catch (SQLException e) {
+			throw new RepositorioException(e);
+		} catch (Exception e) {
+			throw new RepositorioException(e);
+		} finally {
+			stmt.close();
+		}
 	}
 
-}
+} 
---
+++ src/br/com/coffeebeans/atividade/IAtividadeDAO.java
@@ -0,0 +1,20 @@
+package br.com.coffeebeans.atividade;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.coffeebeans.exception.AtividadeNaoEncontradaException;
+import br.com.coffeebeans.exception.RepositorioException;
+
+public interface IAtividadeDAO {
+	
+	public void cadastrar(Atividade atividade) throws SQLException,  
+	RepositorioException;
+	
+	public List<Atividade> getLista() throws SQLException, RepositorioException;
+	
+	public Atividade procurar (int id) throws SQLException, RepositorioException;
+	
+	public void atualizar(Atividade atividade) throws AtividadeNaoEncontradaException,
+	SQLException, RepositorioException;
+}
